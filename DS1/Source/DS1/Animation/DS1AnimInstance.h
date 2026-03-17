75de7304162534bfd924c76bb8f98da816d8b23d
LSW <leemsh1236@gmail.com>
2026-03-17 11:38:20 +0900

    Completed Player Animation

---
+++ DS1/Source/DS1/Animation/DS1AnimInstance.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "DS1AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DS1_API UDS1AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UDS1AnimInstance();
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = Reference)
+	TObjectPtr<class ACharacter> Character;
+
+	UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = Reference)
+	TObjectPtr<class UCharacterMovementComponent> MovementComponent;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = MovementData)
+	FVector Velocity = FVector::ZeroVector;
+
+	UPROPERTY(BlueprintReadOnly, Category = MovementData)
+	float GroundSpeed = 0.0f;
+
+	UPROPERTY(BlueprintReadOnly, Category = MovementData)
+	bool bShouldMove = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = MovementData)
+	bool bIsFalling = false;
+};
